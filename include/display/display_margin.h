665bc955c6a066164cd9d10fd275e45c253c8e14
HeroHoekstra <herohoekstra06@gmail.com>
2026-03-30 18:19:22 +0200

    feat: add display margin overloads

--- include/display/display_margin.h
+++ include/display/display_margin.h
@@ -22,9 +22,32 @@ class DisplayMargin
      * @param top The top margin in pixels
      * @param right The right margin in pixels
      * @param bottom The bottom margin in pixels
+     * @note Use negative numbers to move the text off-screen
      */
     DisplayMargin(int8_t left, int8_t top, int8_t right, int8_t bottom)
     : mLeft(left), mTop(top), mRight(right), mBottom(bottom)
+    {}
+
+    /**
+     * @brief Constructs the margins which will be used on the screen
+     * @param horizontal The horizontal margin (left and right)
+     * @param vertical The vertical margin (top and bottom)
+     * @note The input is NOT the sum of the margins; so a horizontal margin of `4px` would be `4px + 4px` is a total margin of 8 pixels
+     * @note Use negative numbers to move the text off-screen
+     */
+    DisplayMargin(int8_t horizontal, uint8_t vertical)
+    : DisplayMargin(horizontal, vertical, horizontal, vertical)
+    {}
+
+    /**
+     * @brief Constructs the margins which will be used on the screen
+     * @param horizontal The horizontal margin (left and right)
+     * @param vertical The vertical margin (top and bottom)
+     * @note The input is NOT the sum of the margins; so a margin of 2px would be 2px left, 2px top, etc.
+     * @note Use negative numbers to move the text off-screen
+     */
+    DisplayMargin(int8_t margin)
+    : DisplayMargin(margin, margin, margin, margin)
     {}
 
 };

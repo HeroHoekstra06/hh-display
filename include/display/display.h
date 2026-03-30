1ed4f0d0c70db8fd3d400c44f77ab6fc9289b21a
HeroHoekstra <herohoekstra06@gmail.com>
2026-03-30 16:36:15 +0200

    feat: add Adafruit display to Display

--- include/display/display.h
+++ include/display/display.h
@@ -1,6 +1,10 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
 #include "display/display_text.h"
 
 
@@ -10,10 +14,12 @@
 class Display
 {
   private:
-    uint8_t mScreenWidth;   ///< The width of the screen
-    uint8_t mScreenHeight;  ///< The height of the screen
-    uint8_t mAddress;       ///< The address of the screen (default is 0x3C, but if that doesn't work, try 0x3D)
-    int8_t mOledReset;      ///< The reset pin for the screen (-1 for no reset pin)
+    Adafruit_SSD1306 mDisplay;  ///< The actual display screen
+
+    uint8_t mScreenWidth;       ///< The width of the screen
+    uint8_t mScreenHeight;      ///< The height of the screen
+    uint8_t mAddress;           ///< The address of the screen (default is 0x3C, but if that doesn't work, try 0x3D)
+    int8_t mOledReset;          ///< The reset pin for the screen (-1 for no reset pin)
 
   public:
     /**
@@ -25,7 +31,8 @@ class Display
     */
     Display(uint8_t width, uint8_t height, int8_t oledReset, uint8_t address=0x3C)
     : mScreenWidth(width), mScreenHeight(height), 
-      mOledReset(oledReset), mAddress(address)
+      mOledReset(oledReset), mAddress(address),
+      mDisplay(Adafruit_SSD1306{width, height, &Wire, oledReset})
     {}
 
 };

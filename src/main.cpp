f285230e3dd06b8835148d2865a66777080d94fa
HeroHoekstra <herohoekstra06@gmail.com>
2026-03-30 17:17:33 +0200

    feat: add simpel print to screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
-#include <Arduino.h>
+#include "display/display.h"
+#include "display/display_text.h"
 
-// put function declarations here:
-int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup() 
+{
+  Serial.begin(115200);
+  delay(1500);
+  
+  Display display{128, 64};
+  display.start();
+  display.clear();
+
+  display.print({"Hello,\nWorld!"});
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop() 
+{
 }

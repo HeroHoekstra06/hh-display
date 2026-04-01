2fd33173d8e13736e2fe3cf2e2e044ad8659489b
HeroHoekstra06 <herohoekstra06@gmail.com>
2026-04-01 16:56:53 +0200

    feat: add display alignment to display

--- include/display/display.h
+++ include/display/display.h
@@ -5,6 +5,7 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+#include "display/display_align.h"
 #include "display/display_text.h"
 #include "display/display_margin.h"
 
@@ -23,6 +24,7 @@ class Display
     int8_t mOledReset;          ///< The reset pin for the screen (-1 for no reset pin)
 
     uint8_t mPrintSize;         ///< The size at which the text is printed
+    DisplayAlign mAlignment;    ///< The alignment of the printed text
     DisplayMargin mMargin;      ///< The margins which will leave a gap between the edge of the screen and the text
 
   public:
@@ -36,7 +38,7 @@ class Display
     Display(uint8_t width, uint8_t height, int8_t oledReset, uint8_t address=0x3C)
     : mScreenWidth(width), mScreenHeight(height), 
       mOledReset(oledReset), mAddress(address),
-      mPrintSize(2), mMargin(0),
+      mPrintSize(2), mMargin(0), mAlignment(DisplayAlign{}),
       mDisplay(Adafruit_SSD1306{width, height, &Wire, oledReset})
     {}
 
@@ -96,6 +98,9 @@ class Display
     /// @note This assumes the default Adafruit character height (`x * 8`)
     uint16_t getCharHeight() const { return mPrintSize * 8; }
 
+    /// @return The alignment of the text of the screen
+    DisplayAlign& getAlignment() { return mAlignment; }
+
     /// @return The margins which will leave a gap between the edge of the screen and the text
     DisplayMargin& getMargin() { return mMargin; }
 
@@ -104,6 +109,9 @@ class Display
     /// @param size Sets the text size of characters that are printed to the screen
     /// @note Any size larger than 5 may cause the letters to be too big
     void setPrintSize(uint8_t size) { mPrintSize = size; }
+
+    /// @param alignment Sets the alignment of the printed text
+    void setAlignment(const DisplayAlign& alignment) { mAlignment = alignment; }
 
     /// @param margin Sets the margins of the screen
     /// @note Any size larger than 5 may create too little room for characters

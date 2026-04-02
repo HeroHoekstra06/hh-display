6497ef46335451704c3e9cf69a0fefd8d50caa8b
HeroHoekstra06 <herohoekstra06@gmail.com>
2026-04-02 18:09:05 +0200

    feat: add display typography class

---
+++ include/display/display_typography.h
@@ -0,0 +1,33 @@
+#ifndef DISPLAY_TYPOGRAPHY_H
+#define DISPLAY_TYPOGRAPHY_H
+
+#include <stdint.h>
+
+
+/**
+ * @brief A helper class for `Display` which determines the gap between lines and characters
+*/
+class DisplayTypography
+{
+  private:
+    uint8_t mLineHeight;  ///< The height of each line in pixels
+    uint8_t mCharGap;     ///< The gap between each character in pixels
+
+  public:
+    /**
+     * @brief Constructs a DisplayTypography object, which determines the gap between characters
+     * @param lineHeight The amount that the Y position of the cursor is incremented with
+     * @param charGap The amount of pixels between each character
+     * @note `lineHeight` is the total line height. 
+     * This means that if a characters height is 8, and the line height is 4, the characters will overlap.
+     * @note `charGap` is only for the X gap between characters, not Y. 
+     * To change the gap between the characters on the Y axis, call `Display::getCharHeight() + desiredGapSize`.
+    */
+    DisplayTypography(uint8_t lineHeight, uint8_t charGap)
+    : mLineHeight(lineHeight), mCharGap(charGap)
+    {}
+
+};
+
+
+#endif//DISPLAY_TYPOGRAPHY_H

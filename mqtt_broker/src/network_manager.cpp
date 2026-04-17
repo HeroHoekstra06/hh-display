d3d0123a34d0faf59ca8c57986e1b2ac572cd927
HeroHoekstra06 <herohoekstra06@gmail.com>
2026-04-17 16:06:52 +0200

    feat: add private network manager

--- mqtt_broker/src/network_manager.cpp
+++ mqtt_broker/src/network_manager.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 
 
+// Public
 bool NetworkManager::start()
 {
   m_server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -70,4 +71,77 @@ bool NetworkManager::sendData(int client_fd, const std::vector<uint8_t>& data)
 {
   ssize_t bytes_sent = send(client_fd, data.data(), data.size(), 0);
   return bytes_sent == static_cast<ssize_t>(data.size());
+}
+
+
+// Private
+void NetworkManager::serverLoop()
+{
+  while (m_running)
+  {
+    int poll_count = poll(m_poll_fds.data(), m_poll_fds.size(), 100);
+
+    if (poll_count < 0)
+    {
+      std::cerr << "Poll error" << std::endl;
+      break;
+    }
+
+    if (poll_count == 0) continue;
+
+    std::lock_guard<std::mutex> lock(m_clients_mutex);
+
+    for (int i = m_poll_fds.size() - 1; i >= 0; --i)
+    {
+      if (m_poll_fds[i].revents & POLLIN)
+      {
+        if (m_poll_fds[i].fd == m_server_fd)
+        {
+          acceptNewClient();
+        }
+        else
+        {
+          handleClientData(i);
+        }
+      }
+    }
+  }
+}
+
+void NetworkManager::acceptNewClient()
+{
+  struct sockaddr_in client_addr;
+  socklen_t addr_len = sizeof(client_addr);
+  int new_socket = accept(m_server_fd, (struct sockaddr*)& client_addr, &addr_len);
+
+  if (new_socket >= 0)
+  {
+    struct pollfd client_poll_fd;
+    client_poll_fd.fd = new_socket;
+    client_poll_fd.events = POLLIN;
+    m_poll_fds.push_back(client_poll_fd);
+
+    std::cout << "New client has connected to FD: '" << new_socket << "'" << std::endl;
+  }
+}
+
+void NetworkManager::handleClientData(size_t poll_index)
+{
+  int client_fd = m_poll_fds[poll_index].fd;
+  std::vector<uint8_t> buffer(4096);
+
+  ssize_t bytes_read = recv(client_fd, buffer.data(), buffer.size(), 0);
+
+  if (bytes_read <= 0)
+  {
+    std::cout << "Client '" << client_fd << "' stopped" << std::endl;
+    close(client_fd);
+    m_poll_fds.erase(m_poll_fds.begin() + poll_index);
+  }
+  else
+  {
+    buffer.resize(bytes_read);
+    if (m_callback) 
+      m_callback(client_fd, buffer);
+  }
 }

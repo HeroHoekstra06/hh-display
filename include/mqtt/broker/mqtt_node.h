bf76626b8daf61d2154732b0a5bcad6ef802b825
HeroHoekstra06 <herohoekstra06@gmail.com>
2026-04-30 13:09:54 +0200

    feat: add subsribers to node

--- include/mqtt/broker/mqtt_node.h
+++ include/mqtt/broker/mqtt_node.h
@@ -4,12 +4,18 @@
 #include <unordered_map>
 #include <vector>
 
+#include "mqtt/broker/mqtt_client.h"
+
 
 class MQTTNode
 {
   protected:
     std::string m_topicString;                              /// The name of this topic
     std::unordered_map<std::string, MQTTNode> m_children;   /// The child nodes of this node
+
+    std::vector<MQTTClient*> m_subscribers;                 /// All clients who are subscribed THIS node
+    std::vector<MQTTClient*> m_childSubscribers;            /// All clients who are subscribed to this node's children
+    std::vector<MQTTClient*> m_generalSubscribers;          /// All clients who are subscribed to all this node's children
 
   public:
     /**
@@ -18,7 +24,8 @@ class MQTTNode
      * @note This `topicString` should be the *name* of the topic, not the entire path
      */
     MQTTNode(std::string topicString)
-    : m_topicString(topicString)
+    : m_topicString(topicString), m_children({}), 
+      m_subscribers({}), m_childSubscribers({}), m_generalSubscribers({})
     {}
 
 };
